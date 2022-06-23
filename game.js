fb304c8fa1ed736741f15e2a4ef31f2f4614db4e
Alex <ajtravis2@gmail.com>
2022-06-23 17:11:36 -0400

    finished checkGuess function, phase 1 complete

---
+++ game.js
@@ -0,0 +1,17 @@
+let secretNumber = 42
+
+function checkGuess(num){
+    if (num > secretNumber) {
+        console.log("Too high")
+        return false;
+    }
+    else if(num < secretNumber){
+        console.log("Too low")
+        return false;
+    }
+    else if(num === secretNumber){
+        console.log("Correct!")
+        return true
+    }
+}
+
